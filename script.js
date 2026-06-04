8d2cc562b677ec3a99a5da3184be7c5303976b02
Isaac Naid <20253ds038@utez.edu.mx>
2026-06-04 14:49:14 -0600

    Trabajo en clase 3

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 console.log("Hola ");
 
-const nombre="isaac";
-
-let apellido="naid";
+const nombre = "isaac";
+let apellido = "naid";
 
 console.log(nombre);
-
 console.log(apellido);
 //nombre="naid";
 
-apellido="bg";
+apellido = "bg";
 console.log(apellido);
 
 
-//Objetos de js
-const persona= {
+// =================================================================
+// Objetos de JavaScript
+// =================================================================
+const persona = {
     nombre : "Isaac",
     apellido : "bg",
     edad : 18,
@@ -24,69 +24,77 @@ const persona= {
         "bebis",
         "nachas"
     ]
-
 };
 
-persona.apellido="Isaac BG";
+persona.apellido = "Isaac BG";
 console.log(persona.nombre);
 console.log("===================Fin Objetos====================");
 
 
-//funciones
+// =================================================================
+// Funciones (Ejemplos de sintaxis comentados)
+// =================================================================
 
-//function saludar(nombre){
+// function saludar(nombre){
 //    return "hola" + nombre;
-//}
+// }
 
-//const saludar=(nombre)=>{
+// const saludar=(nombre)=>{
 //    return "hola" + nombre;
-//}
+// }
 
-//const saludar=nombre=>{
-//   return "hola" + nombre;
-//}
+// const saludar=nombre=>{
+//    return "hola" + nombre;
+// }
 
-
-const saludar=nombre=>
-   "hola " + nombre;
+const saludar = nombre => "hola " + nombre;
 
 const saludo = saludar(persona.apellido);
 console.log(saludo);
 console.log("===================Fin Objetos====================");
+
+
+// =================================================================
+// Listas y Métodos de Arrays (Map)
+// =================================================================
 console.log("===================Listas====================");
 
 const listaNumeros = [
-
     5, 10, 3, 8, 9
-
 ];
 
 console.log(listaNumeros);
-listaNumeros  [3]= 7;
+listaNumeros[3] = 7;
 console.log(listaNumeros);
 
 const numerosMultiplicados = listaNumeros.map(
-    (numeroEnElQueVoy)=>{
+    (numeroEnElQueVoy) => {
         return numeroEnElQueVoy * 3;
     }
 );
 console.log(numerosMultiplicados);
 
 const numerosEnObjetos = listaNumeros.map(
-    (numeroEnElQueVoy)=>{
+    (numeroEnElQueVoy) => {
         return {
             valor : numeroEnElQueVoy
-        }
+        };
     }
 );
 console.log(numerosEnObjetos);
-// funcion filter
-const numerosX = [90,70,30,10,50];
+
+
+// =================================================================
+// Método Filter
+// =================================================================
+const numerosX = [90, 70, 30, 10, 50];
 const numerosFiltrados = numerosX.filter(
     numX => numX > 50
 );
 console.log(numerosFiltrados);
-const persona = [
+
+// CORRECCIÓN: Se cambió de 'persona' a 'personas' para evitar el SyntaxError
+const personas = [
     {
         nombre: "Isaac",
         edad : 18
@@ -100,3 +108,26 @@ const persona = [
         edad : 18
     },
 ];
+
+const personasMayores = personas.filter(
+    (personasX) => {
+        return personasX.edad > 17;
+    }
+);
+console.log(personasMayores);
+
+
+// =================================================================
+// Método Reduce
+// =================================================================
+const numerosY = [3, 6, 9, 12, 16];
+
+const sumaNumero = numerosY.reduce(
+    (variablePersistente, elemento) => variablePersistente + elemento,
+    0
+);
+
+console.log(sumaNumero);
+
+// Fin del script de práctica de JavaScript estándar
+// =================================================================
